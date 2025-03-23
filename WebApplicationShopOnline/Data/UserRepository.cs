dff25cd60f810679e299ce1bec68beabc0e1c580
Stas Persaev <persaev10@mail.ru>
2025-03-23 20:57:46 +0300

    Создал класс UserRepository для хранения пользователей

---
+++ WebApplicationShopOnline/Data/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Data
+{
+	public class UserRepository
+	{
+		static List<User> users = new List<User>
+		{
+			new User("Stas", "Admin1", "Aa123456", "+79999999", "email@com"),
+			new User("Anna", "Admin2", "Aa123456", "+7229339", "email@com"),
+			new User("Alan", "Alan4ik", "Aa123456", "+444444", "alan254@google.com"),
+		};
+
+		public List<User> GetAll()
+		{
+			return users;
+		}
+
+		public User? TryGetById(Guid idUser)
+		{
+			return users.FirstOrDefault(user => user.Id == idUser);
+		}
+	}
+}
