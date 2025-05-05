caef03fc0b00fef6bf874a45a800b57339703c2a
Stas Persaev <persaev10@mail.ru>
2025-05-06 00:28:29 +0300

    начали делать валидацию

--- WebApplicationShopOnline/Controllers/AdminController.cs
+++ WebApplicationShopOnline/Controllers/AdminController.cs
@@ -4,9 +4,9 @@ using WebApplicationShopOnline.Models;
 
 namespace WebApplicationShopOnline.Controllers
 {
-    public class AdminController : Controller
-    {
-        readonly IProductsRepository productsRepository;
+	public class AdminController : Controller
+	{
+		readonly IProductsRepository productsRepository;
 
 		public AdminController(IProductsRepository productsRepository)
 		{
@@ -14,23 +14,35 @@ namespace WebApplicationShopOnline.Controllers
 		}
 
 		public IActionResult Products()
-        {
-            var products = productsRepository.GetAll();
-            return View(products);
-        }
+		{
+			var products = productsRepository.GetAll();
+			return View(products);
+		}
 
-        [HttpGet]
-        public IActionResult AddProduct()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult AddProduct()
+		{
+			return View();
+		}
 
 
-        [HttpPost]
-        public IActionResult AddProduct(Product product)
-        {
-            productsRepository.Add(product);
-            return RedirectToAction("Products");
-        }
+		[HttpPost]
+		public IActionResult AddProduct(Product product)
+		{
+			productsRepository.Add(product);
+			return RedirectToAction("Products");
+		}
+
+		public IActionResult EditProduct(Guid id)
+		{
+			var product = productsRepository.TryGetById(id);
+			return View(product);
+		}
+
+		[HttpPost]
+		public IActionResult EditProduct(Product product)
+		{
+
+		}
 	}
 }
